1276b70088f894b84df48d4ac8b18e38e5655d1f
David Pierce <pie2ce@gmail.com>
2025-10-26 14:55:50 -0400

    two sum: brute force

--- dsa/arrays_hashing/two_sum/go/main.go
+++ dsa/arrays_hashing/two_sum/go/main.go
@@ -1,1 +1,12 @@
 package main
+
+func TwoSum(nums []int, target int) []int {
+	for i, iValue := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if iValue+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
