c46654f0b5227d64ce7c8619f7e418be618e81b1
David Pierce <pie2ce@gmail.com>
2025-10-27 01:22:03 -0400

    best time to buy: two pointers

--- dsa/sliding_window/best_time_to_buy_and_sell_stock/python/main.py
+++ dsa/sliding_window/best_time_to_buy_and_sell_stock/python/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from python!")
+def main(prices: list[int]) -> int:
+    low, high = 0, 1
+    profit = 0
+
+    while high < len(prices):
+        if prices[high] > prices[low]:
+            if prices[high] - prices[low] > profit:
+                profit = prices[high] - prices[low]
+        else:
+            low = high
+        high += 1
+    
+    return profit
 
 
 if __name__ == "__main__":
-    main()
+    main([2,3,4,5,6,7])
