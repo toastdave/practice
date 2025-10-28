0296613dc659d5b7c7ac64a16331bf4569be3406
David Pierce <pie2ce@gmail.com>
2025-10-27 23:34:49 -0400

    valid parenthese: hashmap

--- dsa/stack/valid_parentheses/python/main.py
+++ dsa/stack/valid_parentheses/python/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from python!")
+def main(s: str) -> bool:
+    if len(s) % 2 != 0: 
+        return False
+
+    stack = []
+    hashmap = {'}':'{', ')':'(', ']': '['}
+
+    for c in s:
+        if c in hashmap:
+            if stack and stack[-1] == hashmap[c]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(c)
+    
+    return True if not stack else False
+
+
 
 
 if __name__ == "__main__":
-    main()
+    main("()")
